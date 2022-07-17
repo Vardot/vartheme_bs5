ac8d6607a283e830f52ddf4f7ecb49c1bdd52a33
Abdallah <a.yassin@vardot.com>
2022-07-17 15:23:09 +0300

    Issue #3296015: Remove twig loop for alert variations and make each one separate component

--- src/components/alert/alert.stories.js
+++ src/components/alert/alert.stories.js
@@ -1,6 +1,7 @@
 export default { title: 'Molecules/Alert' };
 
 import alert from './alert.twig';
+import DrupalAttribute from 'drupal-attribute';
 
 const types = [
   'primary',
@@ -13,14 +14,90 @@ const types = [
   'dark',
 ];
 
-const content = [];
-types.map((item, index) => (
-  content.push(`A simple ${item} alert—check it out!`)
-));
-
-export const Alert = () => (
+export const primary = () => (
   alert({
-    heading: "alert heading",
-    variations: types
+    attributes: new DrupalAttribute(),
+    type: 'primary',
+    dismissible: true,
+    utility_classes: [],
+    heading: 'Lorem Ipsum',
+    content: 'A simple primary alert—check it out!'
   })
-);
+)
+
+export const secondary = () => (
+  alert({
+    attributes: new DrupalAttribute(),
+    type: 'secondary',
+    dismissible: true,
+    utility_classes: [],
+    heading: 'Lorem Ipsum',
+    content: 'A simple secondary alert—check it out!'
+  })
+)
+
+export const success = () => (
+  alert({
+    attributes: new DrupalAttribute(),
+    type: 'success',
+    dismissible: true,
+    utility_classes: [],
+    heading: 'Lorem Ipsum',
+    content: 'A simple success alert—check it out!'
+  })
+)
+
+export const danger = () => (
+  alert({
+    attributes: new DrupalAttribute(),
+    type: 'danger',
+    dismissible: true,
+    utility_classes: [],
+    heading: 'Lorem Ipsum',
+    content: 'A simple danger alert—check it out!'
+  })
+)
+
+export const warning = () => (
+  alert({
+    attributes: new DrupalAttribute(),
+    type: 'warning',
+    dismissible: true,
+    utility_classes: [],
+    heading: 'Lorem Ipsum',
+    content: 'A simple warning alert—check it out!'
+  })
+)
+
+export const info = () => (
+  alert({
+    attributes: new DrupalAttribute(),
+    type: 'info',
+    dismissible: true,
+    utility_classes: [],
+    heading: 'Lorem Ipsum',
+    content: 'A simple info alert—check it out!'
+  })
+)
+
+export const light = () => (
+  alert({
+    attributes: new DrupalAttribute(),
+    type: 'light',
+    dismissible: true,
+    utility_classes: [],
+    heading: 'Lorem Ipsum',
+    content: 'A simple light alert—check it out!'
+  })
+)
+
+export const dark = () => (
+  alert({
+    attributes: new DrupalAttribute(),
+    type: 'dark',
+    dismissible: true,
+    utility_classes: [],
+    heading: 'Lorem Ipsum',
+    content: 'A simple dark alert—check it out!'
+  })
+)
