a3debe988d93e851aff6ac58464fc90fc00b3a6f
Abdallah <a.yassin@vardot.com>
2022-07-19 10:16:49 +0300

    Issue #3296015 by Abdullah Yassin: Modify storybook configuration to add and enable Actions and Controls

---
+++ .storybook/addons.js
@@ -0,0 +1,10 @@
+// .storybook/manager.js
+import '@storybook/addon-viewport/register';
+import '@storybook/addon-actions/register';
+// import '@storybook/addon-knobs/dist/register';
+import '@storybook/addon-links/register';
+import '@storybook/addon-backgrounds/register';
+// import '@storybook/addon-storysource/register';
+import '@storybook/addon-docs/register';
+// import '@storybook/addon-a11y/register';
+import '@storybook/addon-controls/register';
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,11 +5,17 @@ module.exports = ({ config }) => {
   // Twig:
   config.module.rules.push({
     test: /\.twig$/,
-    use: [
-      {
-        loader: 'twig-loader',
-      },
-    ],
+    use: [{
+      loader: 'twig-loader',
+    }, ],
   });
+
+  // Sass:
+  config.module.rules.push({
+    test: /\.scss$/,
+    use: ['style-loader', 'css-loader', 'sass-loader'],
+    include: path.resolve(__dirname, '../'),
+  });
+
   return config;
 };
