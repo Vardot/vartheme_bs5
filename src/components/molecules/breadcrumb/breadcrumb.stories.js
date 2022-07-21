0927da95cf076f6586ef70a2a6a6f1e9eea1db68
Abdallah <a.yassin@vardot.com>
2022-07-21 03:43:39 +0300

    Issue #3296015: Fix Breadcrumb component twig and add docs and settings for its storybook

--- src/components/molecules/breadcrumb/breadcrumb.stories.js
+++ src/components/molecules/breadcrumb/breadcrumb.stories.js
@@ -1,25 +1,28 @@
-export default { 'title' : 'Molecules/Breadcrumb' };
-
 import breadcrumb from './breadcrumb.twig';
+import twigCode from '!!raw-loader!./breadcrumb.twig';
+const breadcrumbSettings = require('./breadcrumb.settings.json');
+import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
 
-const data = [
-  {
-    title: 'Home',
-    url: '#'
+export default {
+  'title' : 'Molecules/Breadcrumb',
+  component: breadcrumb,
+  parameters: {
+    docs: {
+      container: DocsContainer,
+      page: DocsPage,
+      source: {
+        code: twigCode,
+      },
+    },
   },
-  {
-    title: 'Library',
-    url: '#'
-  },
-  {
-    title: 'Data',
-    url: '#'
-  }
-]
+};
 
-export const Breadcrumb = () => (
+export const Breadcrumb = (args) => (
   breadcrumb({
     utility_classes: [],
-    breadcrumb: data
+    breadcrumb: breadcrumbSettings.breadcrumb.data,
+    props: {...args},
   })
 );
+
+Breadcrumb.args = {};
