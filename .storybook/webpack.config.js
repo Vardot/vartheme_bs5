438033d2869b4b1dc29bf7bad776a141c67a7277
Rajab Natshah <rajabn@gmail.com>
2022-07-25 17:08:07 +0300

    Issue #3299786: Configure Twig Namespaces to work for nested components and Drupal template system

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,20 +2,48 @@ const path = require('path');
 
 module.exports = ({ config }) => {
 
-  // Twig:
+  // Twig
   config.module.rules.push({
-    test: /\.twig$/,
-    use: [{
-      loader: 'twig-loader',
-    }, ],
+    test: /\.(html.twig|twig)$/,
+    use: [
+      {
+        loader: 'twig-loader',
+        options: {
+          twigOptions: {
+            namespaces: {
+              vartheme_bs5: path.resolve(__dirname, '../', 'src/components'),
+              atoms: path.resolve(__dirname, '../', 'src/components/atoms'),
+              molecules: path.resolve(__dirname, '../', 'src/components/molecules'),
+              organisms: path.resolve(__dirname, '../', 'src/components/organisms'),
+              templates: path.resolve(__dirname, '../', 'src/components/templates'),
+              pages: path.resolve(__dirname, '../', 'src/components/pages'),
+            },
+          },
+        },
+      },
+    ],
   });
 
   // Sass:
   config.module.rules.push({
-    test: /\.scss$/,
+    test: /\.s[ac]ss$/i,
     use: ['style-loader', 'css-loader', 'sass-loader'],
     include: path.resolve(__dirname, '../'),
   });
 
+  // Row text loader.
+  config.module.rules.push({
+    test: /\.txt$/i,
+    use: [
+      {
+        loader: 'raw-loader',
+        options: {
+          esModule: false,
+        },
+      },
+    ],
+  });
+
   return config;
 };
+
