dbb834440eede011958cf821cc518ec55f0fcf33
Rajab Natshah <rajabn@gmail.com>
2022-07-28 17:50:28 +0300

    Issue #3299997: fix badge.config.yml and badge.stories.js

--- src/components/atoms/badge/badge.stories.js
+++ src/components/atoms/badge/badge.stories.js
@@ -1,13 +1,13 @@
 import config from "./badge.config.yml";
 import badge from './badge.twig';
 import twigCode from '!!raw-loader!./badge.twig';
+import DrupalAttribute from 'drupal-attribute';
 import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
 
 export default {
   title : config.title,
   component: badge,
   parameters: {
-    options: { showPanel: false },
     docs: {
       container: DocsContainer,
       page: DocsPage,
@@ -22,11 +22,27 @@ export default {
       options: config.color.options,
       description: config.color.description,
       defaultValue: {summary: config.color.default},
-      table: config.color.table,
+      table: config.color.table
+    },
+    tag: {
+      control: { type: "select" },
+      options: config.tag.options,
+      description: config.tag.description,
+      defaultValue: {summary: config.tag.default},
+      table: config.tag.table
     },
     content: {
-      content: { control: 'text' }
+      content: { control: 'text' },
+      description: config.content.description,
+      defaultValue: {summary: config.content.default},
+      table: config.content.table
     },
+    url: {
+      content: { control: 'text' },
+      description: config.url.description,
+      defaultValue: {summary: config.url.default},
+      table: config.url.table
+    }
   }
 };
 
@@ -34,15 +50,17 @@ export const Badge = (args) => (
   badge({
     attributes: new DrupalAttribute(),
     utility_classes: [],
-    tag: "span",
-    background_color: args.color ? args.color : 'text-bg-secondary',
-    content: args.content,
-    url: "",
+    color: args.color ? args.color : 'text-bg-secondary',
+    tag: args.tag ? args.tag : 'span',
+    content: args.content ? args.content : 'Badge',
+    url: args.url ? args.url : '',
     props: {...args},
   })
 );
 
 Badge.args = {
-  label: "Badge",
-  color: "text-bg-secondary"
+  color: "text-bg-secondary",
+  tag: 'span',
+  content: "Badge",
+  url: '',
 };
