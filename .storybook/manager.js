c3fce4fe1c918a21908dcf8c1b7c439759d6dc35
Abdallah <a.yassin@vardot.com>
2022-07-30 13:06:24 +0300

    Issue #3300866: Enable accessibility tab on storybook

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -6,5 +6,5 @@ import '@storybook/addon-links/register';
 import '@storybook/addon-backgrounds/register';
 // import '@storybook/addon-storysource/register';
 import '@storybook/addon-docs/register';
-// import '@storybook/addon-a11y/register';
+import '@storybook/addon-a11y/register';
 import '@storybook/addon-controls/register';
