df163971cd84cd919d64aa4048f954d2ab1a5a78
Abdallah <a.yassin@vardot.com>
2022-07-30 21:07:37 +0300

    Issue #3300902: make radio and checkbox inputs link to new input component path and change remove_form_control variable name to form_control

--- src/components/organisms/form/input/input.stories.js
+++ src/components/organisms/form/input/input.stories.js
@@ -1,58 +1,58 @@
-import config from './input.config.yml';
-import input from './input.twig';
-import twigCode from '!!raw-loader!./input.twig';
-import DrupalAttribute from 'drupal-attribute';
-import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
+// import config from './input.config.yml';
+// import input from './input.twig';
+// import twigCode from '!!raw-loader!./input.twig';
+// import DrupalAttribute from 'drupal-attribute';
+// import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
 
-export default {
-  title : config.title,
-  component: input,
-  options: { showPanel: true },
-  parameters: {
-    docs: {
-      container: DocsContainer,
-      page: DocsPage,
-      source: {code: twigCode},
-      description: config.description,
-      iframeHeight: config.height
-    },
-  },
-  argTypes: {
-    size: {
-      control: { type: "select" },
-      options: config.size.options,
-      description: config.size.description,
-      defaultValue: {summary: config.size.default},
-      table: config.size.table,
-    },
-    placeholder: {
-      control: { type: 'text' },
-      description: config.placeholder.description,
-      defaultValue: {summary: config.placeholder.default},
-      table: config.placeholder.table,
-    },
-    disabled: {
-      control: { type: 'boolean' },
-      description: config.disabled.description,
-      defaultValue: {summary: config.disabled.default},
-      table: config.disabled.table,
-    }
-  },
-};
+// export default {
+//   title : config.title,
+//   component: input,
+//   options: { showPanel: true },
+//   parameters: {
+//     docs: {
+//       container: DocsContainer,
+//       page: DocsPage,
+//       source: {code: twigCode},
+//       description: config.description,
+//       iframeHeight: config.height
+//     },
+//   },
+//   argTypes: {
+//     size: {
+//       control: { type: "select" },
+//       options: config.size.options,
+//       description: config.size.description,
+//       defaultValue: {summary: config.size.default},
+//       table: config.size.table,
+//     },
+//     placeholder: {
+//       control: { type: 'text' },
+//       description: config.placeholder.description,
+//       defaultValue: {summary: config.placeholder.default},
+//       table: config.placeholder.table,
+//     },
+//     disabled: {
+//       control: { type: 'boolean' },
+//       description: config.disabled.description,
+//       defaultValue: {summary: config.disabled.default},
+//       table: config.disabled.table,
+//     }
+//   },
+// };
 
-export const Input = (args) => {
-  return (
-    input({
-      attributes: new DrupalAttribute(),
-      size: args.size ? args.size : '',
-      disabled: args.disabled ? 'disabled' : '',
-      placeholder: args.placeholder ? args.placeholder : ''
-    })
-  )
-}
+// export const Input = (args) => {
+//   return (
+//     input({
+//       attributes: new DrupalAttribute(),
+//       size: args.size ? args.size : '',
+//       disabled: args.disabled ? 'disabled' : '',
+//       placeholder: args.placeholder ? args.placeholder : ''
+//     })
+//   )
+// }
 
-Input.args = {
-  size: "",
-  disabled: false,
-  placeholder: "Placeholder text"
-};
+// Input.args = {
+//   size: "",
+//   disabled: false,
+//   placeholder: "Placeholder text"
+// };
