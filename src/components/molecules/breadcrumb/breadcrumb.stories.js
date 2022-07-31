bb825ec89a737bd45a9e0e6dad52d6c2fc9d48b8
Rajab Natshah <rajabn@gmail.com>
2022-07-31 14:38:26 +0300

    Issue #3296015: Change breadcrumb from settings.json to config.yml

--- src/components/molecules/breadcrumb/breadcrumb.stories.js
+++ src/components/molecules/breadcrumb/breadcrumb.stories.js
@@ -1,26 +1,27 @@
+import config from './breadcrumb.config.yml'
 import breadcrumb from './breadcrumb.twig';
 import twigCode from '!!raw-loader!./breadcrumb.twig';
-const breadcrumbSettings = require('./breadcrumb.settings.json');
 import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
 
 export default {
-  'title' : 'Molecules/Breadcrumb',
+  'title' : config.title,
   component: breadcrumb,
+  options: { showPanel: false },
   parameters: {
     docs: {
       container: DocsContainer,
       page: DocsPage,
-      source: {
-        code: twigCode,
-      },
+      source: {code: twigCode},
+      description: config.description,
+      iframeHeight: config.height
     },
-  },
+  }
 };
 
 export const Breadcrumb = (args) => (
   breadcrumb({
     utility_classes: [],
-    breadcrumb: breadcrumbSettings.breadcrumb.data,
+    breadcrumb: config.data,
     props: {...args},
   })
 );
