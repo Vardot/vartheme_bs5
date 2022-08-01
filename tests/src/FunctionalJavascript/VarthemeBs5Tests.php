86d4e355a73b5b24333680d9ebbd60641248c3a7
git <git@3644742.no-reply.drupal.org>
2022-08-01 11:37:09 +0300

    Issue #3290337 by Project Update Bot: Automated Drupal 10 compatibility fixes

--- tests/src/FunctionalJavascript/VarthemeBs5Tests.php
+++ tests/src/FunctionalJavascript/VarthemeBs5Tests.php
@@ -47,7 +47,7 @@ class VarthemeBs5Tests extends WebDriverTestBase {
   /**
    * {@inheritdoc}
    */
-  protected function setUp() {
+  protected function setUp(): void {
     parent::setUp();
 
     // Insall the Claro admin theme.
