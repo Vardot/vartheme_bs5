8c98cbb0f77539c0034a278cfcf12f117632f7b1
Abdallah <a.yassin@vardot.com>
2022-08-27 00:22:12 +0300

    Issue #3305768: Merge the column sizes together instead of specifying the size of each column individually

--- src/components/templates/layout/fourColumnsLayout/fourColumnsLayout.story.js
+++ src/components/templates/layout/fourColumnsLayout/fourColumnsLayout.story.js
@@ -1,120 +1,113 @@
-import config from './fourColumnsLayout.config.yml';
-import fourColumnsLayout from './fourColumnsLayout.twig';
-import twigCode from '!!raw-loader!./fourColumnsLayout.twig';
-import DrupalAttribute from 'drupal-attribute';
-import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
-import './fourColumnsLayout.css';
+import config from "./fourColumnsLayout.config.yml";
+import fourColumnsLayout from "./fourColumnsLayout.twig";
+import twigCode from "!!raw-loader!./fourColumnsLayout.twig";
+import DrupalAttribute from "drupal-attribute";
+import { DocsPage, DocsContainer } from "@storybook/addon-docs/blocks";
+import { getColumnsSizeClasses } from "../../../../helpers";
+import "./fourColumnsLayout.css";
 
 export default {
-  title : config.title,
+  title: config.title,
   component: fourColumnsLayout,
   options: { showPanel: true },
   parameters: {
     docs: {
       container: DocsContainer,
       page: DocsPage,
-      source: {code: twigCode},
+      source: { code: twigCode },
       description: config.description,
-      iframeHeight: config.height
+      iframeHeight: config.height,
     },
   },
   argTypes: {
     row: {
-      control: { type: 'boolean' },
+      control: { type: "boolean" },
       description: config.row.description,
-      defaultValue: {summary: config.row.default},
+      defaultValue: { summary: config.row.default },
       table: config.row.table,
     },
     container: {
-      control: { type: 'boolean' },
+      control: { type: "boolean" },
       description: config.container.description,
-      defaultValue: {summary: config.container.default},
+      defaultValue: { summary: config.container.default },
       table: config.container.table,
     },
     columnOneContent: {
-      content: { control: 'text' },
+      content: { control: "text" },
       description: config.column_1_content.description,
-      defaultValue: {summary: config.column_1_content.default},
+      defaultValue: { summary: config.column_1_content.default },
       table: config.column_1_content.table,
     },
     columnTwoContent: {
-      content: { control: 'text' },
+      content: { control: "text" },
       description: config.column_2_content.description,
-      defaultValue: {summary: config.column_2_content.default},
+      defaultValue: { summary: config.column_2_content.default },
       table: config.column_2_content.table,
     },
     columnThreeContent: {
-      content: { control: 'text' },
+      content: { control: "text" },
       description: config.column_3_content.description,
-      defaultValue: {summary: config.column_3_content.default},
+      defaultValue: { summary: config.column_3_content.default },
       table: config.column_3_content.table,
     },
     columnFourContent: {
-      content: { control: 'text' },
+      content: { control: "text" },
       description: config.column_4_content.description,
-      defaultValue: {summary: config.column_4_content.default},
+      defaultValue: { summary: config.column_4_content.default },
       table: config.column_4_content.table,
     },
-    columnOneSize: {
+    columnsSize: {
       control: { type: "select" },
-      options: config.column_1.options,
-      description: config.column_1.description,
-      defaultValue: {summary: config.column_1.default},
-      table: config.column_1.table,
+      options: config.columns_size.options,
+      description: config.columns_size.description,
+      defaultValue: { summary: config.columns_size.default },
+      table: config.columns_size.table,
     },
-    columnTwoSize: {
-      control: { type: "select" },
-      options: config.column_2.options,
-      description: config.column_2.description,
-      defaultValue: {summary: config.column_2.default},
-      table: config.column_2.table,
-    },
-    columnThreeSize: {
-      control: { type: "select" },
-      options: config.column_3.options,
-      description: config.column_3.description,
-      defaultValue: {summary: config.column_3.default},
-      table: config.column_3.table,
-    },
-    columnFourSize: {
-      control: { type: "select" },
-      options: config.column_4.options,
-      description: config.column_4.description,
-      defaultValue: {summary: config.column_4.default},
-      table: config.column_4.table,
-    }
   },
 };
 
 export const FourColumnsLayout = (args) => {
-  return (
-    fourColumnsLayout({
-      attributes: new DrupalAttribute(),
-      col_attributes: new DrupalAttribute(),
-      row: args.row && 'row',
-      container: args.container && 'container',
-      content: args.content,
-      container_classes: [],
-      row_classes: [],
-      columns: {
-        col1: {attributes: new DrupalAttribute(), size: `varbase-col ${args.columnOneSize}`, content: args.columnOneContent},
-        col2: {attributes: new DrupalAttribute(), size: `varbase-col ${args.columnTwoSize}`, content: args.columnTwoContent},
-        col3: {attributes: new DrupalAttribute(), size: `varbase-col ${args.columnThreeSize}`, content: args.columnThreeContent},
-        col4: {attributes: new DrupalAttribute(), size: `varbase-col ${args.columnFourSize}`, content: args.columnFourContent},
+  const sizeClasses = getColumnsSizeClasses(args.columnsSize);
+
+  return fourColumnsLayout({
+    attributes: new DrupalAttribute(),
+    col_attributes: new DrupalAttribute(),
+    row: args.row && "row",
+    container: args.container && "container",
+    content: args.content,
+    container_classes: [],
+    row_classes: [],
+    columns: {
+      col1: {
+        attributes: new DrupalAttribute(),
+        size: `varbase-col ${sizeClasses[0]}`,
+        content: args.columnOneContent,
       },
-    })
-  )
-}
+      col2: {
+        attributes: new DrupalAttribute(),
+        size: `varbase-col ${sizeClasses[1]}`,
+        content: args.columnTwoContent,
+      },
+      col3: {
+        attributes: new DrupalAttribute(),
+        size: `varbase-col ${sizeClasses[2]}`,
+        content: args.columnThreeContent,
+      },
+      col4: {
+        attributes: new DrupalAttribute(),
+        size: `varbase-col ${sizeClasses[3]}`,
+        content: args.columnFourContent,
+      },
+    },
+  });
+};
 
 FourColumnsLayout.args = {
   row: true,
   container: true,
-  columnOneContent: 'Column one content',
-  columnTwoContent: 'Column two content',
-  columnThreeContent: 'Column three content',
-  columnFourContent: 'Column four content',
-  columnOneSize: 'col-3',
-  columnTwoSize: 'col-3',
-  columnThreeSize: 'col-3',
-  columnFourSize: 'col-3',
+  columnOneContent: "Column one content",
+  columnTwoContent: "Column two content",
+  columnThreeContent: "Column three content",
+  columnFourContent: "Column four content",
+  columnsSize: "25_25_25_25",
 };
