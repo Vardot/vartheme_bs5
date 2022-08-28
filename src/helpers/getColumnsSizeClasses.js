2c2531662e564ca7da4a534224b6fdff9d363db7
Abdallah <a.yassin@vardot.com>
2022-08-28 10:14:51 +0300

    Issue #3306045: Add screen size as a parameter to inject it with columns size

--- src/helpers/getColumnsSizeClasses.js
+++ src/helpers/getColumnsSizeClasses.js
@@ -1,30 +1,32 @@
 /**
  * @param {array} sizes An array of columns sizes like: 50_50, 25_50_25, 25_25_25_25
+ * @param {string} screenSize Screen size prefix: xxl, xl, lg, md, sm
+ *
  * @returns {array} An array of bootstrap classes
  */
 
-export const getColumnsSizeClasses = (sizes) => {
+export const getColumnsSizeClasses = (sizes, screenSize) => {
   const sizesArr = sizes.split("_");
   const classes = [];
   sizesArr.map((size) => {
     switch (size) {
       case "16":
-        classes.push("col-2");
+        classes.push(`col-${screenSize}-2`);
         break;
       case "25":
-        classes.push("col-3");
+        classes.push(`col-${screenSize}-3`);
         break;
       case "33":
-        classes.push("col-4");
+        classes.push(`col-${screenSize}-4`);
         break;
       case "50":
-        classes.push("col-6");
+        classes.push(`col-${screenSize}-6`);
         break;
       case "67":
-        classes.push("col-8");
+        classes.push(`col-${screenSize}-8`);
         break;
       case "75":
-        classes.push("col-9");
+        classes.push(`col-${screenSize}-9`);
         break;
       case "100":
         classes.push(`col-${screenSize}-12`);
