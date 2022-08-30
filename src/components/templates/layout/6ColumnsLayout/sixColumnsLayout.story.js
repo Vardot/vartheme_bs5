1b2ae3d01b6dd83c598cb07272ad89aa08a62b7c
Abdallah <a.yassin@vardot.com>
2022-08-30 11:57:43 +0300

    Issue #3305769: Change twig structure based on new row and container components changes

--- src/components/templates/layout/6ColumnsLayout/sixColumnsLayout.story.js
+++ src/components/templates/layout/6ColumnsLayout/sixColumnsLayout.story.js
@@ -132,10 +132,11 @@ export const _6ColumnsLayout = (args) => {
 
   return sixColumnsLayout({
     attributes: new DrupalAttribute(),
+    container_attributes: new DrupalAttribute(),
+    row_attributes: new DrupalAttribute(),
     col_attributes: new DrupalAttribute(),
-    row: args.row && "row",
-    container: args.container && "container",
-    content: args.content,
+    row: args.row,
+    container: args.container,
     container_classes: [],
     row_classes: [],
     columns: columnsData
