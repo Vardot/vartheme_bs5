d4a7bec9b4457faed1c6d4ab973f6458f6c6beb3
Abdallah <a.yassin@vardot.com>
2022-08-31 16:56:44 +0300

    Issue #3307104: Make storybook accept JSX stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,14 @@
-import { configure } from '@storybook/html';
+import { configure } from "@storybook/html";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
 
-import Twig from 'twig';
-import twigDrupal from 'twig-drupal-filters';
+import Twig from "twig";
+import twigDrupal from "twig-drupal-filters";
 
 twigDrupal(Twig);
 
-configure(require.context('../src/components', true, /\.stories|.story\.js$/), module);
+configure(
+  require.context("../src/components", true, /\.stories|.story\.jsx$/),
+  module
+);
