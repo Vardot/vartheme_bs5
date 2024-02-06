6b2c78102324990ac9af1e9e3bd66a669c3190bc
Rajab Natshah <rajabn@gmail.com>
2024-02-06 18:29:38 +0300

    Issue #3419517: Remove the progress bar library, as it will be overridden, compiled and attached by the varbase_components:progress-bar component

--- webpack.config.components.js
+++ webpack.config.components.js
@@ -18,6 +18,7 @@ module.exports = {
     // 'atoms/accordion-item/accordion-item': ['./components/atoms/accordion-item/accordion-item.scss'],
     // 'atoms/alert/alert': ['./components/atoms/alert/alert.scss'],
     // 'atoms/pagination/pagination': ['./components/atoms/pagination/pagination.scss'],
+    // 'atoms/progress-bar/progress-bar': ['./components/atoms/progress-bar/progress-bar.scss'],
     // 'atoms/taxonomy/taxonomy': ['./components/atoms/taxonomy/taxonomy.scss'],
     // 'molecules/breadcrumb/breadcrumb': ['./components/molecules/breadcrumb/breadcrumb.scss'],
     // 'molecules/tabs/tabs': ['./components/molecules/tabs/tabs.scss'],
