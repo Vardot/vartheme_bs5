aa1a8ef8675c602a0b2d2e0d22fb815e6d19b5a8
Rajab Natshah <rajabn@gmail.com>
2024-08-18 18:12:14 +0300

    Issue #3462276: Add ckeditor5.bootstrap.base.css file to ckeditor5-stylesheets in the vartheme_bs5.info.yml file. with .ck-content wrapper class

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
     "base/vartheme.base": ["./scss/base/vartheme.base.scss"],
     "base/bootstrap-rtl.base": ["./scss/base/bootstrap-rtl.base.scss"],
     "base/vartheme-rtl.base": ["./scss/base/vartheme-rtl.base.scss"],
+    "base/ckeditor5.bootstrap.base": ["./scss/base/ckeditor5.bootstrap.base.scss"],
     // Layout
     "layout/edge2edge.layout": ["./scss/layout/edge2edge.layout.scss"],
     "layout/equal-height.layout": ["./scss/layout/equal-height.layout.scss"],
