1ce1a3ac3f6fc41066c47651c63e65a07ce8635c
Rajab Natshah <rajabn@gmail.com>
2024-09-02 11:19:28 +0300

    Issue #3448563: Comment any use of deprecated font-awesome icon library

--- webpack.config.init.js
+++ webpack.config.init.js
@@ -29,16 +29,17 @@ module.exports = merge(webpackConfig, {
           to: path.resolve(__dirname, "js/bootstrap/util"),
           context: "node_modules/bootstrap/js/dist/util",
         },
-        {
-          from: path.resolve(__dirname, "node_modules/@fortawesome/fontawesome-free/css/all.min.css"),
-          to: path.resolve(__dirname, "fonts/fontawesome-free/css"),
-          context: "node_modules/@fortawesome/fontawesome-free/css",
-        },
-        {
-          from: path.resolve(__dirname, "node_modules/@fortawesome/fontawesome-free/webfonts/*"),
-          to: path.resolve(__dirname, "fonts/fontawesome-free/webfonts"),
-          context: "node_modules/@fortawesome/fontawesome-free/webfonts",
-        },
+        // Uncomment to use the fontawesome library.
+        // {
+        //   from: path.resolve(__dirname, "node_modules/@fortawesome/fontawesome-free/css/all.min.css"),
+        //   to: path.resolve(__dirname, "fonts/fontawesome-free/css"),
+        //   context: "node_modules/@fortawesome/fontawesome-free/css",
+        // },
+        // {
+        //   from: path.resolve(__dirname, "node_modules/@fortawesome/fontawesome-free/webfonts/*"),
+        //   to: path.resolve(__dirname, "fonts/fontawesome-free/webfonts"),
+        //   context: "node_modules/@fortawesome/fontawesome-free/webfonts",
+        // },
         {
           from: path.resolve(__dirname, "node_modules/bootstrap-icons/font/*"),
           to: path.resolve(__dirname, "fonts/bootstrap-icons/font/"),
